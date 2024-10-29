5514b0477eccc2f6675325d03bb4fb9ee96941d8
Petrushka <daniilprokopovic12@gmail.com>
2024-10-29 16:08:31 -0400

    The first stage on a way of creation

---
+++ bot.js
@@ -0,0 +1,42 @@
+const { Bot } = require('grammy');
+const { emojiParser, Reactions } = require('@grammyjs/emoji');
+
+const bot = new Bot('7081370541:AAHbwRj2bPR2iYIYgc7OmyuvEHRGTU0TKwE');
+bot.use(emojiParser());
+
+// The list with different emoji names that will be used in a random order
+const emojiSet = ['skull', 'clown_face', 'goblin', 'ghost', 'alien', 'alien_monster', 'brain', 'duck','pancakes', 'jack_o_lantern'];
+
+console.log(emojiSet.length);
+
+bot.command('start', async (ctx) => {
+	const username = ctx.update.message.from.username;
+	await ctx.reply(`Welcome, ${username}\nType /help to see all available commands`);
+});
+
+bot.command('help', async (ctx) => {
+	await ctx.reply('/start - begin conversation\n/help - see available commands\n/react - get a reaction');
+});
+
+bot.command('react', async (ctx) => {
+	let reactionKey = Math.floor(Math.random() * emojiSet.length);
+	console.log(reactionKey)
+	let randReact = emojiSet[reactionKey];
+
+	await ctx.replyWithEmoji`${randReact}`;
+});
+
+bot.on('message', async (ctx) => {
+	let userMessage = ctx.update.message.text;
+	let answer;
+
+	if (userMessage.toLowerCase().includes('heart')) {
+		answer = ctx.emoji`${'purple_heart'}`;
+	} else {
+		answer = ctx.emoji`I'm sorry, but I'm very unskilled bot and, due to it, cannot reply to your messages yet ${'downcast_face_with_sweat'}`;
+	}
+
+	await ctx.reply(answer);
+});
+
+bot.start();
